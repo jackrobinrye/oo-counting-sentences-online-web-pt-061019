dbc80e5f46e4927df922a02d209c94b2adfa3aa7
Jack Atherton <jackrrye@gmail.com>
2019-06-15 18:20:38 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,8 @@ class String
   end
 
   def count_sentences
-    self.split(/\...|\.|\?|\!/).delete_if { |x| x.size < 2 }.size
+    self.split(/\...|\.|\?|\!/).size
+    #.delete_if { |x| x.size < 2 }.size
   end
   
 end
