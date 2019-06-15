0ac1cc86d9a73cf1e290146d1811a8e8e30e6084
Jack Atherton <jackrrye@gmail.com>
2019-06-15 18:25:34 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,7 @@ class String
 
   def count_sentences
     self.split(/\...|\!!|\.|\?|\!/).size
+    #the following makes the code more complex and better able to handle extended punctuation like !!! and ... without manually putting each one into redux (redux?)
     #.delete_if do |x| 
      # x.size < 2
     #end.size
